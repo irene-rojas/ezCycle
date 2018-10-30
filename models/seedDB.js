e06f6c4cfa679835886587907b674de94de6b8f6
Irene R <irene@smallwondermedia.com>
2018-10-30 11:52:26 -0400

    first entry

--- models/seedDB.js
+++ models/seedDB.js
@@ -10,6 +10,9 @@ const ezCycleSeed = [
     {
         name: "Household Hazardous Waste & Electronics Collection (City of Alexandria, VA)",
         address: "3224 Colvin St, Alexandria, VA 22314",
-        
+        days: "Saturday: 7:30am - 3:30pm (except holidays), Monday: 7:30am - 3:30pm (except holidays)",
+        url: "https://www.alexandriava.gov/tes/solidwaste/info/default.aspx?id=19206",
+        lat: "38.806933",
+        long: "-77.086211"
     }
 ];
