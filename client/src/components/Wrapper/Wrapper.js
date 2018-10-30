c829c90a91c67cbb603b2ba35ba681a9ef558eeb
Lauren <lmcdono17@gmail.com>
2018-10-30 14:05:13 -0400

    another push

--- client/src/components/Wrapper/Wrapper.js
+++ client/src/components/Wrapper/Wrapper.js
@@ -3,9 +3,15 @@ import "./Wrapper.css";
 
 const Wrapper = () => (
 
-<div className="jumbotron">
-<h1 className="text-center">CLICKY GAME</h1>
-  </div>
+    <div className="jumbotron">
+        <div className="row">
+            <div className="col-md-12">
+                <h1 className="text-center">ezCycle</h1>
+                <h2>Recycling done easy. We find centers for you!</h2>
+                <button className="btn btn-primary">SUBMIT</button>
+            </div>
+        </div>
+    </div>
 
 )
 
