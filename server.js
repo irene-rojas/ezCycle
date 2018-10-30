c6bc4b501d95cc9265501a11d387ff7d3f8d06c5
irene-rojas <41927051+irene-rojas@users.noreply.github.com>
2018-10-30 11:05:00 -0400

    Heroku deploy (#1)
    * heroku deployment
    
    * heroku, pt2

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/recycling"
+);
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
