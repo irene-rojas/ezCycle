9502662d75b87887d6eaec9cf883a0744d48bb47
Lmcdono1 <40602801+Lmcdono1@users.noreply.github.com>
2018-10-30 14:59:31 -0400

    Laur (#7)
    * more front end file changes
    
    * another push
    
    * tuesday front end

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -17,7 +17,9 @@ const Footer = () => (
                     
 
 
-                    <button className="btn btn-primary">SUBMIT</button>
+
+                    <button className="btn">SUBMIT</button>
+
                 </div>
 
             </div>
--- client/src/components/Wrapper/Wrapper.js
+++ client/src/components/Wrapper/Wrapper.js
@@ -3,9 +3,14 @@ import "./Wrapper.css";
 
 const Wrapper = () => (
 
-<div className="jumbotron">
-<h1 className="text-center">CLICKY GAME</h1>
-  </div>
+    <div className="jumbotron">
+        
+                <h1 className="text-center" id="wrapper-heading">ezCycle</h1>
+                <h2 id="wrapper-subheading">RECYCLING DONE EASY IN YOUR NEIGHBORHOOD</h2>
+                <button className="btn">RECYCLE NOW</button>
+            
+    </div>
+
 
 )
 
