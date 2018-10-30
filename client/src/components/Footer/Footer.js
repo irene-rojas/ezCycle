86badbad1d3fc0b53c40294f549fabcab9a063fa
Lauren <lmcdono17@gmail.com>
2018-10-30 13:45:05 -0400

    more front end file changes

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,11 +2,38 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => (
+
     <div className="footer">
-    <div className="bottom">
-    Gotta remember 'em all!
-    </div>
-    </div>
-)
+        <div className="container">
+            <div className="row">
+                <div className="col-md-5" id="about-connect">
+                    <h2>ezCycle</h2>
+                    <p>ezCycle is an organization committed to helping users find recycling centers near them. We are constantly looking for new facilities and local events. Follow, like, and spread the word about exCycle!</p>
+
+                </div>
+                <div className="col-md-5 col-md-offset-1" id="contact-connect">
+                    <h2>ADD A FACILITY OR EVENT</h2>
+                    <p>We are always looking for new recycling facilities or events to add to our database. Please fill out this form to submit a new location.</p>
+                    
+
+
+                    <button className="btn btn-primary">SUBMIT</button>
+                </div>
+
+            </div>
+            <div className="row">
+                <div className="col-md-12" id="copyright">
+                    <p>Copyright &copy; 2018</p>
+                </div>
+            </div>
+        </div>
+
+    </div> //footer end
+
+
+
+
+
+);
 
 export default Footer;
---
+++ client/src/components/Wrapper/Wrapper.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Wrapper.css";
+
+const Wrapper = () => (
+
+<div className="jumbotron">
+<h1 className="text-center">CLICKY GAME</h1>
+  </div>
+
+)
+
+export default Wrapper;
