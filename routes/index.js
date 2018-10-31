afb3a30e5688fc792cdd4e532da4bc89528d564b
Leah Oubouzar <32522325+LOubouzar@users.noreply.github.com>
2018-10-31 11:54:22 -0400

    Routing2 (#13)
    * Added express routing and db controller functions
    
    * fixed client .gitignore to include node_modules
    
    * fixed client .gitignore to include node_modules
    
    * removed package-lock.json
    
    * node modules deletion
    
    * routing and api controllers

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const router = require("express").Router();
+const listingRoutes = require("./listing");
+
+// API Routes
+router.use("/listing", listingRoutes);
+
+// If no Listing routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+//Server Dependencies
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+// Serve Static Assests on build production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require('path');
@@ -9,12 +12,15 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const mongoose = require('mongoose');
+//Mongo Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recycling", { useNewUrlParser: true });
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/recycling", { useNewUrlParser: true }
-);
+//Hook Express up to routes
+const routes = require("./routes");
+app.use(routes);
 
-
+//Server Start and Listen Verification
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+app.listen(PORT, function () {
+  console.log(`ezCycle server now listening on PORT ${PORT}!`)
+});
