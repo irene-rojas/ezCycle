f5776df5b785004faec2ef12b1bbdbb05d918de2
Lauren <lmcdono17@gmail.com>
2018-11-05 10:25:13 -0500

    updated contact form

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -12,15 +12,14 @@ const Cards = () => (
                 <div className="col-md-4" >
                     <img className="icons" src={icon3} alt="battery"></img>
                     <h3>BATTERIES</h3>
-                    <p>Rechargeable batteries eventually lose the ability to hold a charge, and recycling them is a must. They can contain corrosive acids and heavy metal elements that can be toxic to the environment. Rechargeable batteries are found in cell phones, tablets, laptops, uninterruptible power supply (UPS) devices, and cordless tools. Wipe your personal information from your phone or computer before recycling!</p>
+                    <p>Rechargeable batteries can contain corrosive acids and heavy metal elements that can be toxic to the environment. Rechargeable batteries are found in cell phones, tablets, laptops, uninterruptible power supply (UPS) devices, and cordless tools. </p>
                 </div>
 
                 <div className="col-md-4">
                 <img className="icons" src={icon2} alt="electronics"></img>
                     <h3>ELECTRONICS</h3>
-                    <p>Many electronic devices are made of valuable substances like plastic, steel, aluminum, copper, gold, and silver that can be recovered. Devices include computer monitors, LCD televisions, keyboards, stereos, video game consoles, printers, and cameras. 
-                        <br></br>
-                        A note about cathode ray tube (CRT) screens and televisions: never disassemble one youself! They can hold a powerful electric charge for a long time. Please confirm that a recycling center accepts them.</p>
+                    <p>Many electronic devices are made of valuable substances like plastic, steel, aluminum, copper, gold, and silver that can be recovered. Devices include computer monitors, LCD televisions, keyboards, stereos, video game consoles, printers, and cameras. </p>
+                    
                 </div>
 
                 <div className="col-md-4">
