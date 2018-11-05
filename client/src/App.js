202bf5534179e3818c8a9bbc8cdd7c0d23afacea
LOubouzar <LOubouzar@gmail.com>
2018-11-05 12:13:04 -0500

    added routing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Page from '../src/components/Page';
 
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Page} />
+        <Route exact path="/listing" component={Page} />
+        <Route exact path="/listing:zipcode" component={Page} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 
-class App extends Component {
+// class App extends Component {
   
-  render() { //part of Component
-    return (
-      <Page />
-    );
-  }
-}
+//   render() { //part of Component
+//     return (
+//       <Page />
+//     );
+//   }
+// }
 
 export default App;
--- client/src/components/Wrapper/Wrapper.js
+++ client/src/components/Wrapper/Wrapper.js
@@ -4,11 +4,11 @@ import "./Wrapper.css";
 const Wrapper = () => (
 
     <div className="jumbotron">
-        
-                <h1 className="text-center" id="wrapper-heading">ezCycle</h1>
-                <h2 id="wrapper-subheading">RECYCLING DONE EASY IN YOUR NEIGHBORHOOD</h2>
-                <button className="btn">RECYCLE NOW</button>
-            
+
+        <h1 className="text-center" id="wrapper-heading">ezCycle</h1>
+        <h2 id="wrapper-subheading">RECYCLING DONE EASY IN YOUR NEIGHBORHOOD</h2>
+        <button className="btn">RECYCLE NOW</button>
+
     </div>
 )
 
