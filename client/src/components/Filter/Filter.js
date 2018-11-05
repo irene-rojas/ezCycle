a3c6ab3ed7f6f1d126f38247824f9f3b59786f62
mhanley00 <mehanley2@gmail.com>
2018-11-04 23:15:21 -0500

    can access search

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -20,12 +20,12 @@ const style = {
 
 const Hit = ({hit}) =>
 <div className="hit">
-<div className="hit-image">
+{/* <div className="hit-image">
 <img src={hit.image}/>
-</div>
+</div> */}
 <div className="hit-content">
-<div className="hit-price">
-${hit.price}
+<div className="hit-lat">
+${hit.lat}
 </div>
 <div className="hit-name">
 <Highlight attributeName="name" hit={hit}/>
@@ -62,9 +62,9 @@ const Content = () =>
     render(){
         return (
     <InstantSearch
-    appId="latency"
-    apiKey="3d9875e51fbd20c7754e65422f7ce5e1"
-    indexName="bestbuy"
+    appId="NVBKVUGOH5"
+    apiKey="2c6162f5b4b1177a3ba0ac6f74508f03"
+    indexName="recyclingTest"
   >
   <SearchBox translations={{placeholder:'Find a Center'}}/>
   <main>
@@ -78,67 +78,3 @@ const Content = () =>
         )}}
 
  export default Filter;
-// =======
-// import React from 'react';
-
-// // the UI component for filtering the subway entrances by subway line
-// export default (props) => {
-//   const { centers, filterCenters } = props;
-
-//   // this is the JSX that will become the Filter UI in the DOM, notice it looks pretty similar to HTML
-//   // notice in the select element onChange is set to the updateFilter method
-//   // thus when a user selects a new subway line to view, the component passes the new filter value
-//   // to the parent component, Map, which reloads the GeoJSON data with the current filter value
-//   return (
-//     <div className="filterRecyclingCenters">
-//       <hr/>
-//       <h3>Find a Recycling Center Near You</h3>
-//       <p>Filter by Zip Code</p>
-//       <select defaultValue="*"
-//         type="select"
-//         name="filterCenters"
-//         onChange={(e) => filterCenters(e)}>
-//           { /* We render the select's option elements by maping each of the values of subwayLines array to option elements */ }
-//           {
-//             centers.map((center, i) => {
-//               return (
-//                   <option value={center} key={i}>{center}</option>
-//                 );
-//             }, this)
-//           }
-//       </select>
-//     </div>
-//   );
-// };
-
-
-// // // the UI component for filtering the subway entrances by subway line
-// // export default (props) => {
-// //   const { lines, filterLines } = props;
-
-// //   // this is the JSX that will become the Filter UI in the DOM, notice it looks pretty similar to HTML
-// //   // notice in the select element onChange is set to the updateFilter method
-// //   // thus when a user selects a new subway line to view, the component passes the new filter value
-// //   // to the parent component, Map, which reloads the GeoJSON data with the current filter value
-// //   return (
-// //     <div className="filterSubwayLines">
-// //       <hr/>
-// //       <h3>Brooklyn Subway Entrances</h3>
-// //       <p>A <a href="http://leafletjs.com/">Leaflet</a> &amp; <a href="https://facebook.github.io/react/">React</a> demo</p>
-// //       <p>Filter Entrances by Subway Line</p>
-// //       <select defaultValue="*"
-// //         type="select"
-// //         name="filterlines"
-// //         onChange={(e) => filterLines(e)}>
-// //           { /* We render the select's option elements by maping each of the values of subwayLines array to option elements */ }
-// //           {
-// //             lines.map((line, i) => {
-// //               return (
-// //                   <option value={line} key={i}>{line}</option>
-// //                 );
-// //             }, this)
-// //           }
-// //       </select>
-// //     </div>
-// //   );
-// // };
