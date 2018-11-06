18fe5b13fc9f7d8d233ceed1c7c399742761d3af
LOubouzar <LOubouzar@gmail.com>
2018-11-06 10:32:27 -0500

    fixed seeds script

--- models/seedDB.js
+++ models/seedDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Listing = require("./listing");
+const db = require("../models");
 
 mongoose.connect(
     process.env.MONGODB_URI ||
@@ -530,14 +531,14 @@ const ezCycleSeed = [
     }
 ];
 
-Listing
-  .remove({})
-  .then(() => Listing.insertMany(ezCycleSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+db.Listing
+ .remove({})
+ .then(() => db.Listing.collection.insertMany(ezCycleSeed))
+ .then(data => {
+   console.log(data.result.n + " records inserted!");
+   process.exit(0);
+ })
+ .catch(err => {
+   console.error(err);
+   process.exit(1);
+ });
